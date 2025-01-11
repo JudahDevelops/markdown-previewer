ce788a168e7e14a42ba9b43202fba6573a0bfdfd
JudahDevelops <judahdevelops@gmail.com>
2025-01-11 08:45:30 +0100

    paused cuz ive run outta time

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Component } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-  return (
-    <>
+  render() {
+    return (
+      <>
+        <Editor />
+        <Previewer />
+      </>
+    )
+  }
+}
+
+class Editor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: ''
+    }
+  }
+
+  render() {
+    const placeholder = "Enter markdown here"
+
+    return (
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h3>Editor</h3>
+        <textarea rows="10" cols="50">
+          {placeholder}
+        </textarea>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    )
+  }
+}
+
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Previewer</h3>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+  }
 }
 
 export default App
