efccb7e1939324d3a80e54be15cce0ddd5e08e8f
JudahDevelops <judahdevelops@gmail.com>
2025-01-12 09:28:06 +0100

    updated a lot of things

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import { useState } from 'react'
 import React, { Component } from 'react';
 import './App.css'
+import marked from 'marked'
 
 class App extends React.Component {
+  render() {
+    return (
+      <MarkdownCompiler />
+    )
+  }
+}
+
+class MarkdownCompiler extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      input: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({ input: event.target.value })
   }
 
   render() {
+
+    const htmlContent = marked(this.state.input);
+
     return (
       <>
-        <Editor />
-        <Previewer />
+        <Editor input={this.state.input} onChange={this.handleChange}/>
+        <Previewer htmlContent={htmlContent}/>
       </>
     )
   }
@@ -20,19 +40,22 @@ class App extends React.Component {
 class Editor extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      input: ''
-    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event) {
+    this.props.onChange(event)
+
   }
 
   render() {
     const placeholder = "Enter markdown here"
 
     return (
-      <div>
+      <div id='editor'>
         <h3>Editor</h3>
-        <textarea rows="10" cols="50">
-          {placeholder}
+        <textarea rows="10" cols="50" onChange={this.handleChange} placeholder={placeholder}>
+
         </textarea>
       </div>
     )
@@ -46,8 +69,13 @@ class Previewer extends React.Component {
 
   render() {
     return (
-      <div>
+      <div id='preview'>
         <h3>Previewer</h3>
+        <div id="preview-box" 
+        dangerouslySetInnerHTML={{
+          __html: this.props.htmlContent, // Render HTML
+        }}
+        ></div>
       </div>
     )
   }
